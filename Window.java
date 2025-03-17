f81f5c274b2ffcf6260bc6b14ed4a15df46b9f5c
Andrew <andrewkogan6@gmail.com>
2025-03-17 13:21:16 -0400

    created a button in the same frame as another event and made it capable of doing something (mostly testing)

--- Window.java
+++ Window.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Window {
-
     private static final JPanel square = new JPanel();
     private static int x = 20;
     private static int y = 200;
@@ -16,53 +15,36 @@ public class Window {
         frame.getContentPane().setLayout(null);
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        JButton button = new JButton("Click Me");
+        button.setSize(100,20);
+        button.setLocation(0,440);
+        frame.add(button);
         frame.add(square);
         square.setBounds(20,200,10,10);
         square.setBackground(Color.RED);
-
         Timer timer = new Timer(1000/60,new MyActionListener());
         timer.start();
         frame.setVisible(true);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                buttonPressed();
+            }
+        } );
+    }
+    public static void buttonPressed(){
+        System.out.println("button clicked!");
     }
 
     public static class MyActionListener implements ActionListener{
-
         @Override
-        public void actionPerformed(ActionEvent arg0) {
+        public void actionPerformed(ActionEvent e) {
             square.setLocation(x++, y--);
 
         }
-
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable(){
-            @Override
-            public void run(){
-                createAndShowGUI();
-                JFrame frame = new JFrame("Button Example");
-                JButton button = new JButton("Click Me");
-
-                // Implement ActionListener using an anonymous inner class
-                button.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        // This code will be executed when the button is clicked
-                        System.out.println("Button Clicked!");
-                        // Add more actions here, e.g., modify other components
-                    }
-                });
-
-                frame.add(button);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-            }
-
-        });
-
-
+        javax.swing.SwingUtilities.invokeLater(Window::createAndShowGUI);
     }
 
 }
